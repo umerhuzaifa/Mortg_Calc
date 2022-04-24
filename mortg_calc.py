e52259c323c0afd68fd986966efcf6eacfd6cd6a
umerhuzaifa <umerhuzaifa@gmail.com>
2022-04-24 07:06:47 -0500

    30 yr fixed rate

---
+++ mortg_calc.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+monthly = []
+
+def mortg_30yrfixed(i_rate, t_rate, s_price, down):
+    for i in rage(30):
+        
+
+
+if __init__=='main':
+    int_rate = 5.11  #%
+    tax_rate = 2.14  #%
+    sale_price = 4e5
+    down_price = 30e3
