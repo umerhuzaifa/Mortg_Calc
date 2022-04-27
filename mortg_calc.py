1f498b1e45a5436c0c3edd440bab727bf9ded521
umerhuzaifa <umerhuzaifa@gmail.com>
2022-04-26 21:57:29 -0500

    Almost complete approach for calculations

--- mortg_calc.py
+++ mortg_calc.py
@@ -5,7 +5,7 @@ monthly30 = []
 
 class Mortg:
     
-    def __init__(self, i_rate, t_rate, s_price, down):
+    def __init__(self, i_rate, t_rate, s_price, down, PMI):
         self.int_rate = i_rate
         self.tax_rate = t_rate
         self.sale_price = s_price
@@ -15,14 +15,21 @@ class Mortg:
         self.total_interest = 0
         self.total_payable = 0
         self.monthly_price = 0
+        self.PMI_rate = PMI
+        self.ratio30mort = 0
+        self.ratio15mort = 0
     def mort_30f(self):
         '''Function to give the bigger picture values: total payment, monthly payment '''
-
-        ratio = (1+self.int_rate)**360        
-        self.monthly_price = (self.sale_price - self.down_payment) \
-            * self.int_rate * (ratio) /(ratio - 1)
+        ## the term (1+r)^n 
+        self.ratio30mort = (1+self.int_rate)**360        
+        
+        self.monthly_price = self.borrowed * self.int_rate * self.ratio30mort/(self.ratio30mort - 1) \
+            + self.tax_rate * self.sale_price + self.borrowed * self.PMI_rate
+        
+        ## (S - D) * (r) * (1+r)^n/((1+r)^n - 1) + S * tax/12
         self.total_payable = 360 * self.monthly_price
-        self.total_interest = self.total_payable - self.borrowed
+        self.total_tax     = 360 * self.tax_rate * self.sale_price
+        self.total_interest = self.total_payable - self.borrowed - self.total_tax - self.borrowed * self.PMI_rate
         for i in range(360):
             self.lst_principal.append(self.monthly_price)
         self.display_results(30)
@@ -30,11 +37,13 @@ class Mortg:
     def mort_15f(self):
         '''Function to give the bigger picture values: total payment, monthly payment '''
 
-        ratio = (1+self.int_rate)**180        
-        self.monthly_price = (self.sale_price - self.down_payment) \
-            * self.int_rate * (ratio) /(ratio - 1)
+        self.ratio15mort = (1+self.int_rate)**180        
+        self.monthly_price = self.borrowed * self.int_rate * self.ratio15mort/(self.ratio15mort - 1) \
+            + self.tax_rate * self.sale_price + self.borrowed * self.PMI_rate
+        ## (S - D) * (r) * (1+r)^n/((1+r)^n - 1) + S * tax/12
         self.total_payable = 180 * self.monthly_price
-        self.total_interest = self.total_payable - self.borrowed
+        self.total_tax     = 180 * self.tax_rate * self.sale_price
+        self.total_interest = self.total_payable - self.borrowed - self.total_tax - self.borrowed * self.PMI_rate
         for i in range(180):
             self.lst_principal.append(self.monthly_price)
         self.display_results(15)
@@ -48,20 +57,29 @@ class Mortg:
         print('| {:,.9}{}| {:,.9}{}| {:,.6}{}|'.format(self.total_payable, 3*' ',self.total_interest,9*' ',self.monthly_price, 8 * ' '))
         print(f"{'|'+15*'-'+'|'+21*'-'+'|'+17*'-'+'|'}")
         months = np.linspace(1, tenor * 12, tenor * 12)
-        fig = plt.figure()
-        ax = fig.add_axes([0,0,1,1])
-        ax.bar(months,self.lst_principal)
-        plt.show()
+##        fig = plt.figure()
+##        ax = fig.add_axes([0,0,1,1])
+##        ax.bar(months,self.lst_principal)
+##        plt.show()
     def month_break(self):
         '''Calculation of breakdown of interest and instalments '''
-        plt.figure()
+##        plt.figure()
             
 
 if __name__=='__main__':
-    int_rate = 5.11/12 * 0.01  #%
-    tax_rate = 2.14 * 0.01 #%
-    sale_price = 4e5
+    mortg_rate = 5.11/12 * 0.01  
+    UIF_rate   = 5.5 /12 * 0.01
+    tax_rate   = 1.9 * 0.01/12
+    PMI        = 1 * 0.01/12
+    
+    sale_price = 3.5e5
     down_price = 30e3
-    home1 = Mortg(int_rate, tax_rate, sale_price, down_price)
+    print('Traditional Mortgage Rate')
+    
+    home1 = Mortg(mortg_rate, tax_rate, sale_price, down_price, PMI)
     home1.mort_30f()
-    home1.mort_15f()
+##    home1.mort_15f()
+    
+    print('UIF Provided Rate')
+    home2 = Mortg(UIF_rate, tax_rate, sale_price, down_price, PMI)
+    home2.mort_30f()
---
+++ mortgage_calc.md
@@ -0,0 +1,12 @@
+Here is how the calculation goes:
+
+S = sale price
+D = down payment
+
+i_rate = fixed interest rate from the mortgage over a month
+      = a typical interest rate (per year)/12
+
+t_rate = property tax over a month
+      = a typical tax rate (per year)/12
+
+monthly payment = S * t_rate  +   (S - D) * (1+i_rate)**360
